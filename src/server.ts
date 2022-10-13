1d8f297f5f20fe3e61201da339e6eedb12cd28d7
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2022-10-12 21:48:10 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,16 @@ import  express  from "express";
 
 const app = express();
 
+
+
+app.get('/games', (request, response) => {
+    return response.json([])
+})
+
+app.post('/ads', (request, response) => {
+    return response.json([])
+})
+
 app.get('/ads', (request, response)=>{
     return response.json([
         {id: 1, name: "Anuncio 1"},
